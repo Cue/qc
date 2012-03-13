2aa21bde397f01426f4aa0b7ae5ea91f4834b4c6
Peter Scott <peter@greplin.com>
2012-03-13 16:19:45 -0700

    Add basic project skeleton.

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""Setup script for qc."""
+
+try:
+  from setuptools import setup
+except ImportError:
+  from distutils.core import setup
+
+
+setup(
+  name = 'qc',
+  version = '0.1',
+  description = 'QuickCheck: randomized testing made trivial.',
+  author = 'Peter Scott',
+  author_email = 'peter@greplin.com',
+  url = 'https://github.com/Greplin/qc',
+  package_dir = {'':'src'},
+  packages = [
+    'qc'
+  ],
+  zip_safe = True,
+  test_suite = 'nose.collector',
+  include_package_data = True,
+  license = 'Apache',
+)
